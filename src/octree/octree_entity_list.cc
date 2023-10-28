b887a5807b2fb0b64fbdbfb859191e02bfc46aee
xv0f <132955532+xv0f@users.noreply.github.com>
2023-10-27 21:21:59 -0700

    0.0.0

--- src/octree/octree_entity_list.cc
+++
@@ -1,8 +0,0 @@
-#include "octree_entity_list.hh"
-
-namespace oLife {
-	template <typename T>
-	OctreeEntityList<T>::OctreeEntityList(std::uint32_t size) :
-		inner(std::vector<OctreeEntityListNode<T> *>(size)),
-		free(size) {};
-}
